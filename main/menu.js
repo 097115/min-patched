c96543b4ef02ba5daea32a1f0161a0e7e0bfdcbc
dsjkvf <dsjkvf@gmail.com>
2025-08-23 04:24:53 +0000

    perf: improve bindings

--- main/menu.js
+++ main/menu.js
@@ -308,7 +308,7 @@ function buildAppMenu (options = {}) {
         {
           label: l('appMenuFullScreen'),
           accelerator: (function () {
-            if (process.platform == 'darwin') { return 'Ctrl+Command+F' } else { return 'F11' }
+            if (process.platform == 'darwin') { return 'Ctrl+Command+F' } else { return 'F10' }
           })(),
           role: 'togglefullscreen'
         }
@@ -322,15 +322,6 @@ function buildAppMenu (options = {}) {
           accelerator: (function () {
             if (process.platform == 'darwin') { return 'Cmd+Alt+I' } else { return 'Ctrl+Shift+I' }
           })(),
-          click: function (item, window) {
-            sendIPCToWindow(window, 'inspectPage')
-          }
-        },
-        // this is defined a second time (but hidden) in order to provide two keyboard shortcuts
-        {
-          label: l('appMenuInspectPage'),
-          visible: false,
-          accelerator: 'f12',
           click: function (item, window) {
             sendIPCToWindow(window, 'inspectPage')
           }
